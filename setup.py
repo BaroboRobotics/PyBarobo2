a84efb52dfb381a13ab9c78597e390709ab4b00f
David Ko <david.dk.ko@gmail.com>
2014-12-18 16:45:19 -0800

    Trying to fix build system...

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ from setuptools import setup, Extension
 import platform
 import sys
 
-origCWD = os.getcwd()
-projDir = os.path.dirname(origCWD)
-projDir = os.path.join(os.path.dirname(origCWD), 'deps', 'baromesh')
-buildDir = os.path.join(origCWD, 'build-ext-'+ platform.platform())
+projDir = os.getcwd()
+# projDir = os.path.dirname(origCWD)
+# projDir = os.path.join(os.path.dirname(origCWD), 'deps', 'baromesh')
+buildDir = os.path.join(projDir, 'build-ext-'+ platform.platform())
 
 try:
     if not os.path.isdir(buildDir):
@@ -48,7 +48,7 @@ if platform.system() == 'Windows':
                 '-DBUILD_SHARED_LIBS=OFF',
                 '-DCMAKE_BUILD_TYPE=Debug',
                 projDir])
-    subprocess.check_call(['mingw32-make', 'baromesh', 'VERBOSE=1'])
+    subprocess.check_call(['mingw32-make', 'VERBOSE=1'])
     libraries+=[ 'msvcr100',
                  'ws2_32',
                  'setupapi']
@@ -59,7 +59,9 @@ if platform.system() == 'Windows':
     data_files = [('linkbot', 
         [ 'dlls/libgcc_s_dw2-1.dll',
           'dlls/libstdc++-6.dll',
-          'dlls/libwinpthread-1.dll'])]
+          'dlls/libwinpthread-1.dll',
+          os.path.join(buildDir, '_linkbot.pyd'), 
+          ])]
 else:
     # Build our C/C++ library into our tempdir staging directory
     if not os.path.exists(os.path.join(buildDir, 'Makefile')):
@@ -70,15 +72,15 @@ else:
                 '-DBUILD_SHARED_LIBS=OFF',
                 '-DCMAKE_BUILD_TYPE=Debug',
                 projDir])
-    subprocess.check_call(['make', 'baromesh', 'VERBOSE=1'])
+    subprocess.check_call(['make', 'VERBOSE=1'])
     boost_libraries = []
     for lib in boost_libs:
         boost_libraries += ['boost_'+lib]
     libraries += boost_libraries
-    data_files = None
+    data_files = [('linkbot', [os.path.join(buildDir, '_linkbot.so')])]
 
 #Go back to our original directory
-os.chdir(origCWD)
+os.chdir(projDir)
 
 if os.environ['BOOST_ROOT'] is None:
     print('Environment variable BOOST_ROOT is not declared. aborting...')
@@ -86,6 +88,7 @@ if os.environ['BOOST_ROOT'] is None:
 
 try:
     setup(name='Linkbot',
+        platforms='blah',
         version='2.0.0',
         description='Barobo Linkbot module',
         author='David Ko',
@@ -93,30 +96,11 @@ try:
         url='http://www.barobo.com',
         packages=['linkbot.demo', 'linkbot'],
         ext_package='linkbot',
-        ext_modules=[Extension('_linkbot', 
-          sources=['src/linkbot_wrapper.cpp'],
-          include_dirs=[
-            os.path.join(os.path.dirname(origCWD), 'deps', 'baromesh', 'include'),
-            os.environ['BOOST_ROOT'],
-            ],
-          library_dirs=[
-            os.path.join(buildDir),
-            os.path.join(buildDir, 'libsfp'),
-            os.path.join(buildDir, 'ribbon-bridge'),
-            os.path.join(buildDir, 'ribbon-bridge-interfaces'),
-            os.path.join(os.environ['BOOST_ROOT'], 'stage', 'lib'),
-            buildDir],
-          libraries=libraries,
-          extra_compile_args=['-g', 
-                              '-DBOOST_ALL_NO_LIB=1', 
-                              '-DBOOST_PYTHON_SOURCE',
-                              '-std=c++11'],
-          extra_link_args=['-g'],
-          )],
+        data_files = data_files,
         package_dir={'':'src'},
         #py_modules=['linkbot._linkbot'],
         zip_safe = False,
-        data_files = data_files,
+        ext_modules=[Extension('__stub', sources=['src/stub.cpp'])]
         )
 except Exception as e:
     print(e)
