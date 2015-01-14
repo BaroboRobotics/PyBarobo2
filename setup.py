e3d086abada0fdb7f9462cf9c785694d4217c5fd
David Ko <david.dk.ko@gmail.com>
2015-01-14 11:47:23 -0800

    Updated version numbers, changelog

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, Extension
 import platform
 import sys
 
-PyLinkbot_Version = '2.0.3'
+PyLinkbot_Version = '2.0.4'
 
 projDir = os.getcwd()
 # projDir = os.path.dirname(origCWD)
