e25d20eb96fdca3cc00c67df23e74ed210d946f9
David Ko <david.dk.ko@gmail.com>
2015-03-18 14:57:26 -0700

    Bumped version numbers

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, Extension
 import platform
 import sys
 
-PyLinkbot_Version = '2.1.1'
+PyLinkbot_Version = '2.1.2'
 
 projDir = os.getcwd()
 # projDir = os.path.dirname(origCWD)
